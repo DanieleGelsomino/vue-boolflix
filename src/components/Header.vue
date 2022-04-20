436bed3ba592d582ffdf951c5dc967546271397d
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-04-20 13:12:06 +0200

    created the starting components

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row"></div>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/style/varstyles";
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row"></div>
+</template>
+
+<script>
+export default {
+  name: "MainComponent",
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/style/varstyles";
+</style>
